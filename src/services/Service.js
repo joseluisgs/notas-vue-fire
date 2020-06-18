5801646e683e90309a510852a511b63674205e3f
joseluisgs <joseluisgonsan@gmail.com>
2020-06-18 13:44:43 +0200

    API URL Heroku

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 // Dirección de la la API a consumir Heroku o donde esté publicado el back
-// const baseURL = 'https://notas-back-nem.herokuapp.com/api/';
+const baseURL = 'https://notas-back-nem.herokuapp.com/api/';
 // Local
-const baseURL = 'http://localhost:8000/api/';
+// const baseURL = 'http://localhost:8000/api/';
 
 export default axios.create({
   baseURL,
