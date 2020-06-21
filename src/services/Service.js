6509a183493d6edf0dbc12ea55f10b26051304f6
joseluisgs <joseluisgonsan@gmail.com>
2020-06-21 13:42:34 +0200

    projectId String

--- src/services/Service.js
+++ src/services/Service.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIRE_API_KEY,
   authDomain: process.env.VUE_APP_FIRE_AUTH_DOMAIN,
   databaseURL: process.env.VUE_APP_FIRE_DATABASE_URL,
-  projectId: process.env.VUE_APP_FIRE_PROJECT_ID,
+  projectId: `${process.env.VUE_APP_FIRE_PROJECT_ID}`,
   storageBucket: process.env.VUE_APP_FIRE_STORAGE_BUCKET,
   messagingSenderId: process.env.VUE_APP_FIRE_MESSAGING_SENDER_ID,
   appId: process.env.VUE_APP_FIRE_APP_ID,
