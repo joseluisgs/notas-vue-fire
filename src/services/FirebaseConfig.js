e7b600dddfc3a1b493d83c211cc730321476f254
joseluisgs <joseluisgonsan@gmail.com>
2020-06-21 14:46:48 +0200

    Config

---
+++ src/services/FirebaseConfig.js
@@ -0,0 +1,11 @@
+// Configuración de firebase si lo quieresponer oculto
+// https://firebase.google.com/docs/web/setup?hl=es
+export default {
+  apiKey: 'AIzaSyDSkTE9_nRcANeFZJXcp_lLg_dXSt2EBoM',
+  authDomain: 'notas-vue.firebaseapp.com',
+  databaseURL: 'https://notas-vue.firebaseio.com',
+  projectId: 'notas-vue',
+  storageBucket: 'notas-vue.appspot.com',
+  messagingSenderId: '330201172452',
+  appId: '1:330201172452:web:f133692114529c39aa1ff1',
+};
